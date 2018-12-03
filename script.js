f1961435a6b790bdb763af61bea68dbfce1bae04
jfelisme <jfelisme@fiu.edu>
2018-12-03 13:21:09 -0500

    set logic for correct and incorrect selection

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,10 +22,20 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     //compare clickColor tp pickedColor
     if (clickedColor === pickedColor) {
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again!";
     }
 
   });
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    // change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
 }
