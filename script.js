1cc7f7c317afd4551c2ef73034dfb110d08f28bd
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-12-03 22:11:08 -0500

    add bar and reset button

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function () { 
+  // generate all new colors
+  colors = genarateRandomColors(6);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  // change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  // change colors of square
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+ });
 
 colorDisplay.textContent = pickedColor;
 
@@ -16,13 +31,13 @@ for (var i = 0; i < squares.length; i++) {
     //compare clickColor tp pickedColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
-      h1.style.backgroundColor = pickedColor;
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again!";
     }
-
   });
 }
 
