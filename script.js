255ca5a5951875e05ec5d12abd0e3cdc3412711e
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-12-03 22:40:30 -0500

    add Easy and hard mode logic

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-var colors = genarateRandomColors(6);
+var numSquares = 6;
+var colors = genarateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyButton");
+var hardButton = document.querySelector("#hardButton");
+
+
+easyButton.addEventListener("click", function () { 
+  hardButton.classList.remove("selected");
+  easyButton.classList.add("selected");
+  numSquares = 3;
+  colors = genarateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++){
+    if(colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardButton.addEventListener("click", function () { 
+  hardButton.classList.add("selected");
+  easyButton.classList.remove("selected");
+  numSquares = 6;
+  colors = genarateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+});
 
 resetButton.addEventListener("click", function () { 
   // generate all new colors
-  colors = genarateRandomColors(6);
+  colors = genarateRandomColors(numSquares);
   // pick a new random color from array
   pickedColor = pickColor();
   // change colorDisplay to match picked color
@@ -18,7 +51,7 @@ resetButton.addEventListener("click", function () {
     squares[i].style.backgroundColor = colors[i];
   }
   h1.style.backgroundColor = "#232323";
- });
+});
 
 colorDisplay.textContent = pickedColor;
 
